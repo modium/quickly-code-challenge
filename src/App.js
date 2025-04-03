1c64afad600281648fbd5477c02b0233dafd8a98
modium <rtql8d@gmail.com>
2025-04-03 17:24:35 -0600

    sets up pages, components and routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Unauthorized from "./pages/Unauthorized";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <h1>Quickly Code Challenge</h1>
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="*" element={<Unauthorized />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Profile.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PaymentDateChecker from "../components/PaymentDateChecker";
+
+export default function Profile() {
+  return (
+    <div>
+      Profile
+      <PaymentDateChecker />
+    </div>
+  );
+}
