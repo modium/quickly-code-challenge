a715b04a287df931a99302cddc7188b113d6d204
modium <rtql8d@gmail.com>
2025-04-03 19:18:06 -0600

    finishes PamentDateChecker component

--- src/components/PaymentDateChecker.js
+++ src/components/PaymentDateChecker.js
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import moment from "moment";
 
 export default function PaymentDateChecker() {
-  return <div>PaymentDateChecker</div>;
+  const [invoiceDueDate, setInvoiceDueDate] = useState(null);
+  const [payCycleDate, setPayCycleDate] = useState(null);
+  const [payDateText, setPayDateText] = useState("");
+
+  useEffect(() => {
+    if (invoiceDueDate !== null && payCycleDate !== null) {
+      let payDate = moment(payCycleDate).format("MMMM Do");
+      setPayDateText("Your invoice pay date will be on " + payDate);
+    }
+  }, [invoiceDueDate, payCycleDate]);
+
+  return (
+    <div>
+      <h3>Payment Date Checker</h3>
+      <h3>Invoice due date: {invoiceDueDate}</h3>
+      <h3>Pay cycle date: {payCycleDate}</h3>
+      <div>
+        <h3>Invoice due date</h3>
+        <input
+          type="date"
+          onChange={(e) => setInvoiceDueDate(e.target.value)}
+        />
+      </div>
+      <div>
+        <h3>Pay cycle date</h3>
+        <input type="date" onChange={(e) => setPayCycleDate(e.target.value)} />
+      </div>
+      <p>{payDateText}</p>
+    </div>
+  );
 }
